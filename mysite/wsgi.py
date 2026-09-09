264b6b7cb29b8783045e239e38ce50b09b8b1350
lemonderon <45392009+lemonderon@users.noreply.github.com>
2026-09-09 09:10:30 +0300

    Enhance WSGI file documentation
    Add documentation for WSGI application usage and deployment.

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,5 +1,8 @@
 """WSGI application for mysite.
 
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
 https://docs.djangoproject.com/en/6.1/howto/deployment/wsgi/
 """
 
